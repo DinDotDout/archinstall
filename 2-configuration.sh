a3540e574f115bdab91f8206f563a8cfffb64b3a
DinDotDout <joandot1998@gmail.com>
2023-09-25 14:25:21 +0200

    First commit for archinstall

---
+++ 2-configuration.sh
@@ -0,0 +1,174 @@
+#!/bin/bash
+
+#   ____             __ _                       _   _             
+#  / ___|___  _ __  / _(_) __ _ _   _ _ __ __ _| |_(_) ___  _ __  
+# | |   / _ \| '_ \| |_| |/ _` | | | | '__/ _` | __| |/ _ \| '_ \ 
+# | |__| (_) | | | |  _| | (_| | |_| | | | (_| | |_| | (_) | | | |
+#  \____\___/|_| |_|_| |_|\__, |\__,_|_|  \__,_|\__|_|\___/|_| |_|
+#                         |___/                                   
+# by Stephan Raabe (2023)
+# ------------------------------------------------------
+clear
+keyboardlayout="us"
+zoneinfo="Europe/Madrid"
+hostname="archdot"
+username="joan"
+
+# ------------------------------------------------------
+# Set System Time
+# ------------------------------------------------------
+ln -sf /usr/share/zoneinfo/$zoneinfo /etc/localtime
+hwclock --systohc
+
+# ------------------------------------------------------
+# Update reflector
+# ------------------------------------------------------
+echo "Start reflector..."
+reflector -c "Spain" -p https -a 3 --sort rate --save /etc/pacman.d/mirrorlist
+
+# ------------------------------------------------------
+# Synchronize mirrors
+# ------------------------------------------------------
+pacman -Syy
+
+# ------------------------------------------------------
+# Install Packages
+# ------------------------------------------------------
+
+while true; do
+  read -p "Do you want any graphics drivers? (Nn (Nvidia), Aa (amd) or Xx (None)): " an
+    case $an in
+        [Nn]* ) 
+            echo "Adding nvidia-dkms"
+            pacman --needed --noconfirm -S nvidia-dkms
+            # ------------------------------------------------------
+            # add nvidia hook
+            # ------------------------------------------------------
+            echo "
+            [Trigger]
+            Operation=Install
+            Operation=Upgrade
+            Operation=Remove
+            Type=Package
+            Target=nvidia-dkms
+            Target=linux
+            # Change the linux part above if a different kernel is used
+
+            [Action]
+            Description=Update NVIDIA module in initcpio
+            Depends=mkinitcpio
+            When=PostTransaction
+            NeedsTargets
+            Exec=/bin/sh -c 'while read -r trg; do case $trg in linux*) exit 0; esac; done; /usr/bin/mkinitcpio -P'
+            " >> /etc/pacman.d/hooks/nvidia.hook
+        break;;
+        [aA]* )
+            echo "Adding xf86-video-amdgpu"
+            pacman --needed --noconfirm -S xf86-video-amdgpu
+        break;;
+        [xX]* )
+            exit;
+        break;;
+        * ) echo "Please answer n, a or x.";;
+    esac
+done
+
+pacman --needed --noconfirm -S grub xdg-desktop-portal-wlr \
+  efibootmgr networkmanager network-manager-applet dialog wpa_supplicant\
+  mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils\
+  gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip \
+  alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion \
+  openssh rsync reflector acpi acpi_call dnsmasq openbsd-netcat ipset firewalld \
+  flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g exa bat htop \
+  ranger neofetch duf xorg xorg-xinit grub-btrfs nvidia-dkms \
+  brightnessctl pacman-contrib git feh curl zsh \
+	alacritty neovim firefox  man-db \
+	man-pages rofi ripgrep telegram-desktop \
+	zip unzip  gtk3 lxappearance-gtk3  ttf-hack zathura \
+	zathura-pdf-mupdf ueberzug ly mlocate lf filelight pavucontrol
+
+
+# ------------------------------------------------------
+# set lang utf8 US
+# ------------------------------------------------------
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+# ------------------------------------------------------
+# Set Keyboard
+# ------------------------------------------------------
+echo "FONT=ter-v18n" >> /etc/vconsole.conf
+echo "KEYMAP=$keyboardlayout" >> /etc/vconsole.conf
+
+# ------------------------------------------------------
+# Set hostname and localhost
+# ------------------------------------------------------
+echo "$hostname" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 $hostname.localdomain $hostname" >> /etc/hosts
+clear
+
+# ------------------------------------------------------
+# Add User
+# ------------------------------------------------------
+echo "Add user $username"
+useradd -m -G wheel $username
+passwd $username
+
+# ------------------------------------------------------
+# Enable Services
+# ------------------------------------------------------
+systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl enable cups.service
+systemctl enable sshd
+systemctl enable avahi-daemon
+systemctl enable reflector.timer
+systemctl enable fstrim.timer
+systemctl enable firewalld
+systemctl enable acpid
+
+# ------------------------------------------------------
+# Grub installation
+# ------------------------------------------------------
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --removable
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# ------------------------------------------------------
+# Add btrfs and setfont to mkinitcpio
+# ------------------------------------------------------
+# Before: BINARIES=()
+# After:  BINARIES=(btrfs setfont)
+sed -i 's/BINARIES=()/BINARIES=(btrfs setfont)/g' /etc/mkinitcpio.conf
+mkinitcpio -p linux
+
+# ------------------------------------------------------
+# Add user to wheel
+# ------------------------------------------------------
+clear
+echo "Uncomment %wheel group in sudoers (around line 85):"
+echo "Before: #%wheel ALL=(ALL:ALL) ALL"
+echo "After:  %wheel ALL=(ALL:ALL) ALL"
+echo ""
+read -p "Open sudoers on press" c
+EDITOR=vim sudo -E visudo
+usermod -aG wheel $username
+
+# ------------------------------------------------------
+# Copy installation scripts to home directory 
+# ------------------------------------------------------
+cp /archinstall/3-after-install.sh /home/$username
+
+clear
+echo "     _                   "
+echo "  __| | ___  _ __   ___  "
+echo " / _' |/ _ \| '_ \ / _ \ "
+echo "| (_| | (_) | | | |  __/ "
+echo " \__,_|\___/|_| |_|\___| "
+echo "                         "
+echo ""
+echo "Please find the following additional installation scripts in your home directory:"
+echo ""
+echo "Please exit & shutdown (shutdown -h now), remove the installation media and start again."
---
+++ 3-after-install.sh
@@ -0,0 +1,65 @@
+clear
+echo "Installing paru!"
+git clone https://aur.archlinux.org/paru-git.git
+ (cd paru-git && makepkg -si)
+echo "DONE!"
+
+echo "Installing Paru pckgs!"
+paru --noconfirm --needed -S neovim-remote stow \
+	catppuccin-gtk-theme-macchiato catppuccin-cursors-mocha \
+	protonup-qt timeshift zram-generaton preload
+echo "DONE!"
+clear
+
+echo "Adding zram"
+if [ -f "/etc/systemd/zram-generator2.conf" ]; then
+    echo "/etc/systemd/zram-generator.conf already exists!"
+else
+	sudo touch /etc/systemd/zram-generator.conf
+	sudo bash -c 'echo "[zram0]" >> /etc/systemd/zram-generator.conf'
+	sudo bash -c 'echo "zram-size = ram / 2" >> /etc/systemd/zram-generator.conf'
+    sudo systemctl daemon-reload
+    sudo systemctl start /dev/zram0
+fi
+echo "DONE!"
+
+clear
+
+git clone git@github.com:DinDotDout/.dotfilesV2.git
+(cd .dotfiles stow base-conf)
+xdg-user-dirs-update
+
+echo "Changing to zsh shell"
+chsh -s /usr/bin/zsh
+zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1 --keep
+echo "Changing to ly display manager"
+dpmpath='ExecStart=/usr/bin/'
+servicename=$(grep $dpmpath /etc/systemd/system/display-manager.service)
+if ! [ -z "$service" ]; then
+	echo "Disabling current display manager service"
+	servicename="${servicename#$dpmpath}.service"
+	systemctl disable $servicename
+fi
+echo "Enabling new display manager service"
+systemctl enable ly.service
+
+while true; do
+  read -p "Do you want to install i3 or hyprland? (iI/hY/Xx(None)): " hin
+    case $hin in
+        [hH]* )
+            echo "Hyprland configuration started."
+            source hyprland-install.sh
+        break;;
+        [iI]* ) 
+            echo "I3 configuration started."
+            source i3-install.sh
+        break;;
+        [nN]* )
+            # exit;
+        break;;
+        * ) echo "Please answer i, h or x.";;
+    esac
+done
+
+echo "Finished"
+echo "All options will be enabled after rebooting"
